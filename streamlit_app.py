cf9105b548921fee705f6d6d7c9f7dd62c6851db
Vijay Gupta <107228934+mevijaygupta2022@users.noreply.github.com>
2023-09-30 13:14:08 -0700

    Update streamlit_app.py
    added Pandas component

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ st.text('🥗 Kale, Spinac & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-range Egg')
 st.text('🥑🍞 Avocoado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(my_fruit_list)
