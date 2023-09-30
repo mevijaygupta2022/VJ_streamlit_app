0c9608b351dd8a1124ba094869e61d14aae7b396
Vijay Gupta <107228934+mevijaygupta2022@users.noreply.github.com>
2023-09-30 13:03:41 -0700

    Update streamlit_app.py
    New lines of code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit
-streamlit.title('My Parents new Healthy Diner')
+import streamlit as st
+st.title('My Parents new Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & blueberry Oatmeal')
+st.text('Kale, Spinac & Rocket Smoothie')
+st.text('Hard-oiled Free-range Egg')
